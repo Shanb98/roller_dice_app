9654951755a82f0759e7e22ae712190b80d890f6
Shanb98 <shanbasnayake@hotmail.com>
2023-12-01 12:51:05 +0530

    final 12/01

--- lib/roller_dice.dart
+++ lib/roller_dice.dart
@@ -7,6 +7,7 @@ class RollerDice extends StatefulWidget {
   @override
   State<RollerDice> createState() => _RollerDiceState();
 }
+const numberOfRounds = 5;
 
 class _RollerDiceState extends State<RollerDice> {
   int diceNumber = 1;
@@ -15,13 +16,47 @@ class _RollerDiceState extends State<RollerDice> {
   int playerTwoWins = 0;
   int playerOneRoud = 1;
   int playerTwoRoud = 1;
- 
+  List<int> playerOneScores = [];
+  List<int> playerTwoScores = [];
+  String whoWins = '';
   playerOneAction() {
     setState(() {
       diceNumber = Random().nextInt(6) + 1;
     });
   }
-
+  updateRound() {
+    setState(() {
+      if (playerOneRoud == playerTwoRoud && round <= numberOfRounds) {
+        if (playerOneScores[round] > playerTwoScores[round]) {
+          playerOneWins += 1;
+        } else if (playerTwoScores[round] > playerOneScores[round]) {
+          playerTwoWins += 1;
+        }
+        round += 1;
+      }
+      if (round == numberOfRounds) {
+        if (playerOneWins > playerTwoWins) {
+          whoWins = 'Player 1 Wins!';
+        } else if (playerTwoWins > playerOneWins) {
+          whoWins = 'Player 2 Wins!';
+        } else {
+          whoWins = 'It\'s a Draw!';
+        }
+      }
+    });
+  }
+  reset() {
+  setState(() {
+    diceNumber = 1;
+    round = 0;
+    playerOneWins = 0;
+    playerTwoWins = 0;
+    playerOneRoud = 1;
+    playerTwoRoud = 1;
+    playerOneScores = [];
+    playerTwoScores = [];
+  });
+  }
   playerTwoAction() {
     setState(() {
       diceNumber = Random().nextInt(6) + 1;
@@ -41,10 +76,27 @@ class _RollerDiceState extends State<RollerDice> {
     }
     return false;
   }
+
   @override
   Widget build(BuildContext context) {
     return Column(
-      
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        round == numberOfRounds
+            ? Column(
+                children: [
+                  const Text('Game Over!',
+                      style: TextStyle(color: Colors.red, fontSize: 28)),
+                  Text(whoWins,
+                      style:
+                          const TextStyle(color: Colors.green, fontSize: 28)),
+                  TextButton(
+                      onPressed: reset,
+                      child: const Text('Start a New Game!',
+                          style: TextStyle(color: Colors.blue, fontSize: 20))),
+                ],
+              )
+            : Column(
                 children: [
                   Text(
                     'Round ${(round + 1)}',
@@ -64,7 +116,9 @@ class _RollerDiceState extends State<RollerDice> {
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       TextButton(
-                          onPressed: playerOneAction,
+                          onPressed: isPlayerOneButtonDisable()
+                              ? null
+                              : playerOneAction,
                           style: TextButton.styleFrom(
                               backgroundColor: Colors.pink,
                               foregroundColor: Colors.white,
@@ -74,7 +128,12 @@ class _RollerDiceState extends State<RollerDice> {
                         width: 20,
                       ),
                       TextButton(
-                          onPressed: playerTwoAction(),
+                          onPressed: isPlayerTwoButtonDisable()
+                              ? null
+                              : () {
+                                  playerTwoAction();
+                                  updateRound();
+                                },
                           style: TextButton.styleFrom(
                               backgroundColor: Colors.pink,
                               foregroundColor: Colors.white,
@@ -83,6 +142,53 @@ class _RollerDiceState extends State<RollerDice> {
                     ],
                   ),
                 ],
-              );
+              ),
+        const SizedBox(
+          height: 20,
+        ),
+        Text(
+          'Player 1: $playerOneWins-$playerTwoWins :Player 2',
+          style: const TextStyle(color: Colors.white, fontSize: 24),
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Column(
+              children: [
+                const Text('Round #',
+                    style: TextStyle(color: Colors.black, fontSize: 20)),
+                for (int r = 1; r <= playerOneScores.length; r++)
+                  Text(
+                    'Round $r',
+                  )
+              ],
+            ),
+            const SizedBox(
+              width: 10,
+            ),
+            Column(
+              children: [
+                const Text('Player 1',
+                    style: TextStyle(color: Colors.black, fontSize: 20)),
+                for (var value in playerOneScores) Text(value.toString())
+              ],
+            ),
+            const SizedBox(
+              width: 10,
+            ),
+            Column(
+              children: [
+                const Text('Player 2',
+                    style: TextStyle(color: Colors.black, fontSize: 20)),
+                for (var value in playerTwoScores) Text(value.toString())
+              ],
+            )
+          ],
+        )
+      ],
+    );
   }
 }
